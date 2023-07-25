a2b49932c64ac06ea5e8732dcf2a843246ab624b
Leonardo <leodesouzauy@gmail.com>
2023-07-25 17:33:46 -0300

    Add footnote about knowledge source.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,11 @@
 export default function Footer() {
   return (
     <footer>
-      <p>
-        Built with ❤ to optimize your Google Fonts requests.
+      <p role="note">
+        Based on <a href="https://www.unicode.org/charts/">The Unicode Standard 15.0</a>, omitting <em>control</em> and <em>reserved</em> characters, and the <a href="https://fonts.google.com/">Google Fonts</a> API.
       </p>
       <p>
-        deglyph &copy; 2023 by <a href='https://leodeslf.com'>
+        Built with ❤ to optimize your Google Fonts requests | deglyph &copy; 2023 <a href="https://github.com/leodeslf">
           leodeslf
         </a>
       </p>
