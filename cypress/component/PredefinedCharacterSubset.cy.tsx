d99c1b4fa33580a28ba5206df63ee12fa55e28cf
Leonardo <leodesouzauy@gmail.com>
2023-08-05 09:49:08 -0300

    Add basic cypress setup.

---
+++ cypress/component/PredefinedCharacterSubset.cy.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PredefinedCharacterSubset from
+  '../../src/components/form/PredefinedCharacterSubset';
+
+describe('PredefinedCharacterSubset', () => {
+  it('renders and has the correct data', () => {
+    // cy.mount(<PredefinedCharacterSubset id={1} label='Arabic' />);
+    // cy.get('.predefined-character-subsets__subset');
+  });
+});
