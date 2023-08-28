ab5c8462ad77fd74f821a6c3a162ba3dcc7906ab
Leonardo de S. Leal F. <leodeslf@gmail.com>
2023-08-28 17:03:07 -0300

    Simplify copy.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer>
       <p role="note">
-        Based on <a href="https://www.unicode.org/charts/">The Unicode Standard 15.0</a>, omitting <em>control</em> and <em>reserved</em> characters, and the <a href="https://fonts.google.com/">Google Fonts</a> API.
+        Based on <a href="https://www.unicode.org/charts/">The Unicode Standard 15.0</a> and the <a href="https://fonts.google.com/">Google Fonts</a> API.
       </p>
       <p>
         Built with ❤ to optimize your Google Fonts requests | fontima &copy; 2023 <a href="https://github.com/leodeslf">
