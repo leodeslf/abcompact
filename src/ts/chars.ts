8fba83a58b1ccca10b52cda592081c7a094cc721
Leonardo de S. Leal F. <leodeslf@gmail.com>
2023-09-09 15:15:27 -0300

    Fix CSS analysis. Refactor due to performance & code readability issues.

--- src/ts/chars.ts
+++ src/ts/chars.ts
@@ -1,185 +1,378 @@
-import predefinedCharacterSubsets from
-  '../json/predefinedCharacterSubsets.json';
+import {
+  generateUnicodeRangesFromCss,
+  getFontFaceRules
+} from "./googleFontsCss";
 
-function getCharactersFromUnicodeRange({ start, end }: UnicodeRange): string {
-  let characters: string = '';
+function getCharsFromUnicodeRange({ start, end }: UnicodeRange): string {
+  let chars: string = '';
   let index: number = parseInt('0x'.concat(start));
   const lastIndex = parseInt('0x'.concat(end));
 
   do {
-    characters = characters.concat(String.fromCodePoint(index));
+    chars = chars.concat(String.fromCodePoint(index));
   } while (++index <= lastIndex)
 
-  return characters;
+  return chars;
 }
 
-/**
- * Similar to a bitmap, this function returns and array of 0s or 1s that
- * represent the availability of each Character Unit: if all the characters from
- * a Unit are included, it counts as included (1); if any character from
- * the Unit is missing, the Unit is missing (0).
- */
-function getCharacterCoverageBitmap(
-  characterUnits: string[],
-  availableCharacters: string[]
-): Bit[] {
-  const characterCoverageBitmap = characterUnits.map(characterUnit => {
-    const characterUnitCharacters = [...characterUnit];
-
-    for (const unitCharacter of characterUnitCharacters) {
-      if (!availableCharacters.includes(unitCharacter)) {
-        return 0;
-      }
-    }
-
-    return 1;
-  });
-
-  if (!characterCoverageBitmap.includes(1)) {
-    throw Error('no required character is available for this font');
-  }
-
-  return characterCoverageBitmap;
-}
-
-function getAvailableCharacterUnits(
-  characterUnits: string[],
-  characterCoverageBitmap: Bit[]
-): string[] {
-  return characterUnits.filter((_, index) =>
-    characterCoverageBitmap[index] === 1);
-}
-
-function getCharacterChunks(characterUnits: string[]): string[] {
-  const encodedCharacterChunks: string[] = [];
-  const amountOfCharacterUnits = characterUnits.length;
-  let characterUnitsIndex: number = 0;
-
-  do {
-    let characterChunk: string = '';
-
-    while (
-      characterChunk.length + characterUnits[characterUnitsIndex]?.length <= 800 &&
-      characterUnitsIndex < amountOfCharacterUnits
-    ) {
-      characterChunk = characterChunk.concat(
-        characterUnits[characterUnitsIndex++]
-      );
-    }
-
-    encodedCharacterChunks.push(characterChunk);
-  } while (characterUnitsIndex < amountOfCharacterUnits)
-
-  return encodedCharacterChunks;
-}
-
-function getEncodedCharacterChunks(characterUnits: string[]): string[] {
-  return getCharacterChunks(characterUnits)
-    .map(characterChunk => encodeURIComponent(characterChunk));
+function getCharsFromUnicodeRanges(unicodeRanges: UnicodeRange[]): string {
+  return unicodeRanges
+    .reduce((chars, unicodeRange) =>
+      chars.concat(getCharsFromUnicodeRange(unicodeRange)),
+      ''
+    );
 }
 
 // Control characters from (Basic) ASCII and Supplement-ASCII.
-const blackListedCharacters = getCharactersFromUnicodeRanges([
+const blackListedChars = getCharsFromUnicodeRanges([
   { start: "0", end: "1F" },
   { start: "80", end: "9F" }
 ]);
 
-function filterBlackListedCharacters(
-  characters: string
-): string {
-  let whiteListedCharacters: string = characters;
+function getWhiteListedChars(chars: string): string {
+  let whiteListedChars: string = chars;
 
-  for (const blackListedCharacter of blackListedCharacters) {
-    if (whiteListedCharacters.includes(blackListedCharacter)) {
-      whiteListedCharacters = whiteListedCharacters
-        .replaceAll(blackListedCharacter, '');
+  for (const blackListedChar of blackListedChars) {
+    if (whiteListedChars.includes(blackListedChar)) {
+      whiteListedChars = whiteListedChars
+        .replaceAll(blackListedChar, '');
     }
   }
 
-  return whiteListedCharacters;
+  return whiteListedChars;
 }
 
-function getCharactersFromUnicodeRanges(unicodeRanges: UnicodeRange[]): string {
-  return unicodeRanges.reduce((characters, unicodeRange) =>
-    characters.concat(getCharactersFromUnicodeRange(unicodeRange)),
-    ''
-  );
+/**
+ * 1. Sort items by the amount of atom as an overall organization.
+ * 2. Re-sort items sharing the same length from first to last atom.
+ * 3. Re-sort by first atom to keep results (emojis) visually homogeneous.
+ * 
+ * Radix-like sorting algorithm.
+ */
+function sortCharMolecules(charMolecules: string[]): string[] {
+  charMolecules.sort((a, b) => [...a].length - [...b].length);
+  const largestRadix = charMolecules.at(-1)?.length as number;
+
+  for (let radix = 1; radix < largestRadix; radix++) {
+    charMolecules.sort((a, b) => {
+      const charAtomsA = [...a];
+      const charAtomsB = [...b];
+      return charAtomsA.length === charAtomsB.length ?
+        (charAtomsA[radix]?.codePointAt(0) || 0) -
+        (charAtomsB[radix]?.codePointAt(0) || 0) :
+        0;
+    });
+  }
+
+  charMolecules.sort((a, b) => {
+    const charAtomsA = [...a];
+    const charAtomsB = [...b];
+    return charAtomsA.length === charAtomsB.length ?
+      (charAtomsA[0].codePointAt(0) as number) -
+      (charAtomsB[0].codePointAt(0) as number) :
+      0;
+  });
+
+  return charMolecules;
 }
 
-function characterUnitDecimalValue(characterUnit: string): number {
-  return Number(
-    [...characterUnit]
-      .map(character => String(character.codePointAt(0)).padStart(6, '0'))
-      .reverse()
-      .join('')
-  );
-}
-
-function sortCharacterUnits(
-  characterUnitA: string,
-  characterUnitB: string
-): number {
-  return (
-    characterUnitDecimalValue(characterUnitA) -
-    characterUnitDecimalValue(characterUnitB)
-  );
+/**
+ * Sorted and unique atoms, an atom can be:
+ * 
+ * - A single-length character or
+ * - A double-length character (does not get divided by iterators).
+ */
+function generateCharAtoms(chars: string): {
+  charAtoms: string[],
+  charAtomToCharAtomIndexMap: CharAtomToCharAtomIndexMap
+} {
+  const charAtoms = sortCharMolecules([...new Set(chars)]);
+  return {
+    charAtoms,
+    charAtomToCharAtomIndexMap: charAtoms
+      .reduce<
+        CharAtomToCharAtomIndexMap
+      >((charAtomToCharAtomIndexMap, charAtom, i) => {
+        charAtomToCharAtomIndexMap[charAtom] = i;
+        return charAtomToCharAtomIndexMap;
+      },
+        {}
+      )
+  };
 }
 
 /**
  * Reference:
  * 
- * - https://www.unicode.org/emoji/charts/
- * - https://www.unicode.org/emoji/charts/full-emoji-list.html
- * - https://www.unicode.org/emoji/charts/emoji-ordering.html
  * - https://www.unicode.org/reports/tr51/#EBNF_and_Regex
  * - https://unicode.org/reports/tr18/#Hex_notation
+ * 
+ * Related:
+ * 
+ * - https://unicode.org/Public/emoji/15.0/
+ * - https://www.unicode.org/emoji/charts/
+ * - https://www.unicode.org/emoji/charts/emoji-ordering.html
+ * - https://www.unicode.org/emoji/charts/emoji-ordering.txt
+ * - https://www.unicode.org/emoji/charts/full-emoji-list.html
+ * - https://www.unicode.org/emoji/charts/full-emoji-list.txt
  */
-const emojisRegex = /\p{RI}\p{RI}|\p{Emoji}(\p{EMod}|\uFE0F\u20E3?|[\u{E0020}-\u{E007E}]+\u{E007F})?(\u200D(\p{RI}\p{RI}|\p{Emoji}(\p{EMod}|\uFE0F\u20E3?|[\u{E0020}-\u{E007E}]+\u{E007F})?))*/gu;
+const emojisOrAnyRegex = /(\p{RI}\p{RI}|\p{Emoji}(\p{EMod}|\uFE0F\u20E3?|[\u{E0020}-\u{E007E}]+\u{E007F})?(\u200D(\p{RI}\p{RI}|\p{Emoji}(\p{EMod}|\uFE0F\u20E3?|[\u{E0020}-\u{E007E}]+\u{E007F})?))*)|[^]/gu;
 
-function getCharacterUnits(characters: string): string[] {
-  const emojis: string[] = (characters.match(emojisRegex) || [])
-    .filter(emoji => emoji.length > 1);
-  let lengthOneCharacters: string = characters;
-
-  for (const emoji of emojis) {
-    lengthOneCharacters = lengthOneCharacters.replace(emoji, '');
-  }
-
-  return [...new Set(
-    lengthOneCharacters
-      .split('')
-      .concat(emojis)
-  )].sort(sortCharacterUnits);
-}
-
-function getCharacterUnitsFromInputElements(
-  customCharactersElement: HTMLTextAreaElement,
-  predefinedCharacterSubsetsElements: HTMLFieldSetElement
-): string[] {
-  const predefinedCharacterSubsetsValue = [
-    ...predefinedCharacterSubsetsElements.getElementsByTagName('input')
-  ]
-    .filter(({ checked }) => checked)
-    .map(({ value }) => getCharactersFromUnicodeRanges(
-      predefinedCharacterSubsets
-        .filter((subset) => subset.id === Number(value))[0].unicodeRanges
-    ))
-    .join('');
-
-  return getCharacterUnits(
-    filterBlackListedCharacters(customCharactersElement.value)
-      .concat((predefinedCharacterSubsetsValue))
+/**
+ * Sorted and unique molecules, a molecule can be:
+ * 
+ * - A single-length character,
+ * - A double-length character (is not divided by iterators), or
+ * - A multi-character sequence (is divided by iterators, e.g.: complex emoji).
+ */
+function generateCharMolecules(chars: string): string[] {
+  return sortCharMolecules(
+    [...new Set(chars.match(emojisOrAnyRegex) as string[])]
   );
 }
 
+function generateCharMoleculeToCharAtomIndicesMap(
+  charAtomToCharAtomIndexMap: CharAtomToCharAtomIndexMap,
+  charMolecules: string[],
+): CharMoleculeToCharAtomIndicesMap {
+  const charMoleculeToCharAtomIndices: CharMoleculeToCharAtomIndicesMap = {};
+  charMolecules.forEach(charMolecule => {
+    const charAtomIndices = sortCharMolecules([...new Set(charMolecule)])
+      .map(charAtom => charAtomToCharAtomIndexMap[charAtom]);
+    charMoleculeToCharAtomIndices[charMolecule] = {
+      charAtomIndices,
+      charAtomIndicesAsString: charAtomIndices.toString()
+    }
+  });
+  return charMoleculeToCharAtomIndices;
+}
+
+function generateCharReport(
+  charMolecules: string[],
+  defaultCss: string,
+  amountOfStyles: number
+): {
+  charMoleculesBitmap: Bit[],
+  charMoleculesAvailable: string[],
+  usedCssFontFaceRules: string
+} {
+  const charAtomToMissingCharMoleculeIndicesMap:
+    CharAtomToCharMoleculeIndicesMap = {};
+  charMolecules.forEach((charMolecule, i) => {
+    const charAtoms = sortCharMolecules([...new Set(charMolecule)]);
+
+    for (const charAtom of charAtoms) {
+      if (!charAtomToMissingCharMoleculeIndicesMap[charAtom]) {
+        charAtomToMissingCharMoleculeIndicesMap[charAtom] = [i];
+      } else {
+        charAtomToMissingCharMoleculeIndicesMap[charAtom].push(i);
+      }
+    }
+  });
+  const fontFaceRules = getFontFaceRules(defaultCss);
+  const amountOfFontFaceRules = fontFaceRules.length;
+  const fontFaceRulesFirstStyle = fontFaceRules
+    .slice(0, amountOfFontFaceRules / amountOfStyles);
+  const amountOfFontFaceRulesPerStyle = fontFaceRulesFirstStyle.length;
+  const usedFontFaceRules: string[] = [];
+
+  // Every style includes the same characters, so loop through the 1ts one only.
+  for (let i = 0; i < amountOfFontFaceRulesPerStyle; i++) {
+    let fontFaceRuleIsUsed: boolean = false;
+    const fontFaceRuleChars = getCharsFromUnicodeRanges(
+      generateUnicodeRangesFromCss(fontFaceRulesFirstStyle[i])
+    );
+
+    // If it has a matching character: it's a used one. Add it for every style.
+    for (const fontFaceRuleChar of fontFaceRuleChars) {
+      if (charAtomToMissingCharMoleculeIndicesMap[fontFaceRuleChar]) {
+        charAtomToMissingCharMoleculeIndicesMap[fontFaceRuleChar] = [];
+
+        if (!fontFaceRuleIsUsed) {
+          for (let j = 0; j < amountOfStyles; j++) {
+            usedFontFaceRules.push(fontFaceRules[
+              i + j * amountOfFontFaceRulesPerStyle
+            ]);
+          }
+
+          fontFaceRuleIsUsed = true;
+        }
+      }
+    }
+  }
+
+  if (usedFontFaceRules.length === 0) {
+    throw Error('no required character is available for this font');
+  }
+
+  const charMoleculesBitmap = Array<Bit>(charMolecules.length).fill(1);
+  new Set(
+    Object
+      .values(charAtomToMissingCharMoleculeIndicesMap)
+      .filter(missingCharMoleculeIndices => missingCharMoleculeIndices.length)
+      .flat()
+  ).forEach(missingCharMoleculeIndices =>
+    charMoleculesBitmap[missingCharMoleculeIndices] = 0
+  );
+  return {
+    charMoleculesBitmap,
+    charMoleculesAvailable: charMolecules
+      .filter((_, i) => charMoleculesBitmap[i] === 1),
+    usedCssFontFaceRules: usedFontFaceRules.join('')
+  };
+}
+
+/**
+ * The parameter `chars` has unique and previously sorted characters.
+ */
+function generateUnicodeRangesFromChars(chars: string): UnicodeRange[] {
+  let codePoints: number[] = [...chars]
+    .map(char => char.codePointAt(0) as number);
+  const singleCodePointRangeBoundaryCharacters = codePoints
+    .filter((codePoint, i, array) =>
+      codePoint - (array?.[i - 1] || -Infinity) > 1 &&
+      (array?.[i + 1] || +Infinity) - codePoint > 1
+    );
+  const allCodePointRangeBoundaryCharacters = codePoints
+    .filter((codePoint, i, array) =>
+      codePoint - (array?.[i - 1] || -Infinity) > 1 ||
+      (array?.[i + 1] || +Infinity) - codePoint > 1
+    );
+  const amountOfRangeBoundaries = allCodePointRangeBoundaryCharacters.length;
+  const unicodeRanges: UnicodeRange[] = [];
+
+  for (let i = 0; i < amountOfRangeBoundaries;) {
+    const start = allCodePointRangeBoundaryCharacters[i].toString(16);
+    let end: string;
+
+    if (
+      singleCodePointRangeBoundaryCharacters.includes(
+        allCodePointRangeBoundaryCharacters[i]
+      ) ||
+      i + 1 === amountOfRangeBoundaries
+    ) {
+      end = start;
+      i++;
+    } else {
+      end = allCodePointRangeBoundaryCharacters[i + 1].toString(16);
+      i += 2;
+    }
+
+    unicodeRanges.push({ start, end });
+  }
+
+  return unicodeRanges;
+}
+
+function generateRequestChunks(
+  charAtoms: string[],
+  charMolecules: string[],
+  charMoleculeToCharAtomIndicesMap: CharMoleculeToCharAtomIndicesMap
+): {
+  charChunks: string[],
+  unicodeRangeChunks: UnicodeRange[][]
+} {
+  const charAtomIndicesAsStringToAssignedChunkIndexMap: {
+    [charAtomIndicesAsString: string]: number
+  } = {};
+  const charAtomIndicesChunks: number[][] = [[]];
+  let amountOfChunks: number = charAtomIndicesChunks.length;
+  const amountOfCharMolecules = charMolecules.length;
+
+  for (let i = 0; i < amountOfCharMolecules; i++) {
+    const charMolecule = charMolecules[i];
+    const {
+      charAtomIndices,
+      charAtomIndicesAsString
+    } = charMoleculeToCharAtomIndicesMap[charMolecule];
+    let charMoleculeIsAssigned: boolean = false;
+    let notAssignedCharAtomIndices: number[] = [...charAtomIndices];
+    let assignableChunkIndex: number = -1;
+
+    for (let j: number = 0; j < amountOfChunks; j++) {
+      // Already assigned.
+      if (charAtomIndicesAsStringToAssignedChunkIndexMap[
+        charAtomIndicesAsString
+      ]) {
+        charMoleculeIsAssigned = true;
+        break;
+      }
+
+      const charAtomIndicesChunk = charAtomIndicesChunks[j];
+
+      // Not assigned, but chunk is full.
+      if (charAtomIndicesChunk.length === 800) {
+        continue;
+      }
+
+      // Check which characters are already assigned if any.
+      for (const charAtomIndex of charAtomIndices) {
+        if (charAtomIndicesChunk.includes(charAtomIndex)) {
+          notAssignedCharAtomIndices = notAssignedCharAtomIndices
+            .filter(notAssignedCharAtomIndex =>
+              notAssignedCharAtomIndex !== charAtomIndex
+            );
+          assignableChunkIndex = j;
+        }
+      }
+
+      // Not assigned characters do not fit, reset values.
+      if (
+        (charAtomIndicesChunk.length +
+          notAssignedCharAtomIndices.length) > 800
+      ) {
+        notAssignedCharAtomIndices = [...charAtomIndices];
+        assignableChunkIndex = -1;
+      }
+
+      // Assign them all to this chunk if none is assigned yet.
+      if (
+        notAssignedCharAtomIndices.length ===
+        charAtomIndices.length
+      ) {
+        assignableChunkIndex = j;
+      }
+    }
+
+    // Some characters need to be assigned, changes need to be made.
+    if (!charMoleculeIsAssigned) {
+      // Missing characters did not fit any chunk, create a new one.
+      if (assignableChunkIndex === -1) {
+        assignableChunkIndex =
+          charAtomIndicesChunks.push([]) - 1;
+        amountOfChunks++;
+      }
+
+      const charAtomIndicesChunk = charAtomIndicesChunks[assignableChunkIndex];
+      charAtomIndicesChunk.push(...notAssignedCharAtomIndices);
+      charAtomIndicesAsStringToAssignedChunkIndexMap[
+        charAtomIndicesAsString
+      ] = assignableChunkIndex;
+    }
+  }
+
+  const charChunks = charAtomIndicesChunks
+    .map(charAtomIndicesChunk =>
+      charAtomIndicesChunk
+        .sort((a, b) => a - b)
+        .map(charAtomIndex => charAtoms[charAtomIndex])
+        .join('')
+    );
+  return {
+    charChunks,
+    unicodeRangeChunks: charChunks.map(generateUnicodeRangesFromChars)
+  };
+}
+
 export {
-  filterBlackListedCharacters,
-  getAvailableCharacterUnits,
-  getCharacterChunks,
-  getCharacterCoverageBitmap,
-  getCharactersFromUnicodeRange,
-  getCharactersFromUnicodeRanges,
-  getCharacterUnits,
-  getCharacterUnitsFromInputElements,
-  getEncodedCharacterChunks
+  generateCharMoleculeToCharAtomIndicesMap,
+  generateCharAtoms,
+  generateCharMolecules,
+  generateCharReport,
+  generateRequestChunks,
+  generateUnicodeRangesFromChars,
+  getCharsFromUnicodeRange,
+  getCharsFromUnicodeRanges,
+  getWhiteListedChars,
+  sortCharMolecules,
 };
