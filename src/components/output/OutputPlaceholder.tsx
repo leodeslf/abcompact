4d965a9223b14b45f414cbc454f5504fde38640e
Leonardo de S. Leal F. <leodeslf@gmail.com>
2023-09-20 18:53:52 -0300

    Set Output Placeholder's role as 'note' instead of 'status.'

--- src/components/output/OutputPlaceholder.tsx
+++ src/components/output/OutputPlaceholder.tsx
@@ -1,7 +1,7 @@
 export default function OutputPlaceholder() {
   return (
     <div className="output-placeholder">
-      <p role="status">
+      <p role="note">
         No results to show...
       </p>
     </div>
